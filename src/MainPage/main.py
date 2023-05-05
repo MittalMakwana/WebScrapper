94ed30413b6b7191df7812f7772a233b1eef584d
Mittal Makwana <mittalmak@gmail.com>
2023-05-04 17:35:54 -0700

    Fixed the regex

--- src/MainPage/main.py
+++ src/MainPage/main.py
@@ -25,9 +25,9 @@ class CardRegex:
     _yr = r'(?P<yr>\d\d\d\d)?'
     _ordinal = r'(st|nd|rd|th)?\s'
     _ep = r'(?P<ep>(?:S\d*(EP?\d*T?O?\d*)?)?)'
-    _part = r'(Part \d)?'
+    _part = r'(Part \d)?\s?'
     _view_type = r'(?P<view_type>3D|V2)\s'
-    _str = rf"{_name}\s(\((?P<dt>(?:{_day}{_ordinal}{_month})?{_yr})\)\s+)\s?{_ep}\s?({_view_type})?{_quality}\s?{_type}(.*)\[{_size}\]$"
+    _str = rf"{_name}\s(\((?P<dt>(?:{_day}{_ordinal}{_month})?{_yr})\)\s+)\s?{_ep}\s?{_part}({_view_type})?{_quality}\s?{_type}(.*)\[{_size}\]$"
     CARD_REGEX = re.compile(_str)
 
     def __init__(self):
